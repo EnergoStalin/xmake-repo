7abb52c68878d75a822c3e82c0d168a08145dbbc
EnergoStalin <ens.stalin@gmail.com>
2024-02-28 23:12:28 +0300

    refactor: rules

--- packages/s/s21rules/cpp.lua
+++
@@ -1,7 +0,0 @@
-function set_cpp_compiler_flags()
-  add_rules('mode.release', 'mode.debug', 'mode.tsan', 'mode.asan')
-  add_cxxflags('-Wall', '-Werror', '-Wextra', '-pedantic')
-  add_toolchains('gcc')
-  set_languages('c++17')
-end
-
