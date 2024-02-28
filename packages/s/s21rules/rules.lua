08c3ab4f8a625f7bb3471ceb69dace4f2085e3fc
EnergoStalin <ens.stalin@gmail.com>
2024-02-29 00:11:04 +0300

    fix: do not use unsupported on_load in rules package

--- packages/s/s21rules/rules.lua
+++ packages/s/s21rules/rules.lua
@@ -1,11 +1,8 @@
 rule('coverage')
   if not is_mode('coverage') then return end
 
-  on_load(function(target)
-    target:add_links('gcov')
-  end)
-
   on_config(function(target)
+    target:add_links('gcov')
     target:add_cxflags('-fprofile-arcs', '-ftest-coverage')
   end)
 rule_end()
