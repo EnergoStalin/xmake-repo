647f4eafa89304c58d6ff7421837dd2dd14445dc
EnergoStalin <ens.stalin@gmail.com>
2024-02-28 23:34:03 +0300

    fix: rules package

---
+++ packages/s/s21rules/rules.lua
@@ -0,0 +1,46 @@
+rule('coverage')
+  if not is_mode('coverage') then return end
+
+  on_load(function(target)
+    target:add_links('gcov')
+  end)
+
+  on_config(function(target)
+    target:add_cxflags('-fprofile-arcs', '-ftest-coverage')
+  end)
+rule_end()
+
+rule('lib/report')
+  if not is_mode('coverage') then return end
+
+  after_run(function(target)
+    for k, v in pairs(target._DEPS) do
+      os.execv('lcov', {
+        '-o', path.join(target:targetdir(), k .. '.info'),
+        '-c', '-d', v:objectdir(),
+      })
+    end
+  end)
+rule_end()
+
+rule('report')
+  if not is_mode('coverage') then return end
+
+  after_run(function(target)
+    os.execv('lcov', {
+      '-o', path.join(target:targetdir(), 'coverage.info'),
+      '-c', '-d', target:objectdir()
+    })
+  end)
+rule_end()
+
+rule('valgrind')
+  if not is_mode('valgrind') then return end
+
+  on_run(function(target)
+    os.execv('valgrind', {
+      '--trace-children=yes', '-s',
+      '--leak-check=full', '--', target:targetfile(),
+    })
+  end)
+rule_end()
