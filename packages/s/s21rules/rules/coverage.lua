a03cb3dfdb526015727d5b196f87a88667d95786
EnergoStalin <ens.stalin@gmail.com>
2024-03-03 15:13:38 +0300

    remove: embedded rules

--- packages/s/s21rules/rules/coverage.lua
+++
@@ -1,8 +0,0 @@
-rule('coverage')
-  if not is_mode('coverage') then return end
-
-  on_config(function(target)
-    target:add_links('gcov')
-    target:add_cxflags('-fprofile-arcs', '-ftest-coverage')
-  end)
-rule_end()
