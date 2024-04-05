a59c678a6cdb01e0eb2bed86b60571674344e286
EnergoStalin <ens.stalin@gmail.com>
2024-04-05 06:03:17 +0300

    feat: qcustomplot

---
+++ packages/q/qcustomplot/xmake.lua
@@ -0,0 +1,20 @@
+package('qcustomplot')
+  set_homepage('https://www.qcustomplot.com/')
+  set_description('Qt C++ widget for plotting and data visualization.')
+
+  add_urls('https://www.qcustomplot.com/release/$(version)/QCustomPlot-source.tar.gz',
+    'https://gitlab.com/ecme2/QCustomPlot')
+
+  add_versions('2.1.1', '5e2d22dec779db8f01f357cbdb25e54fbcf971adaee75eae8d7ad2444487182f')
+
+  on_install(function(package)
+    io.writefile('xmake.lua', [[
+      add_rules('mode.debug', 'mode.release')
+      set_languages('cxx17')
+      target('qcustomplot')
+        set_kind('static')
+        add_files('qcustomplot-source/qcustomplot.cpp')
+        add_headerfiles('qcustomplot-source/qcustomplot.h', { public = true })
+    ]])
+    import('package.tools.xmake').install(package)
+  end)
