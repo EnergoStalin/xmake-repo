5b4054710e291f82807c7a3a2d2252ced7a85dbe
EnergoStalin <ens.stalin@gmail.com>
2024-04-16 23:50:35 +0300

    fix: god help me download right version

--- packages/q/qcustomplot/xmake.lua
+++ packages/q/qcustomplot/xmake.lua
@@ -2,8 +2,7 @@ package('qcustomplot')
   set_homepage('https://www.qcustomplot.com/')
   set_description('Qt C++ widget for plotting and data visualization.')
 
-  add_urls('https://www.qcustomplot.com/release/$(version)/QCustomPlot-source.tar.gz',
-    'https://gitlab.com/ecme2/QCustomPlot')
+  add_urls('https://www.qcustomplot.com/release/$(version)/QCustomPlot-source.tar.gz')
 
   add_versions('2.1.1', '5e2d22dec779db8f01f357cbdb25e54fbcf971adaee75eae8d7ad2444487182f')
 
@@ -15,6 +14,7 @@ package('qcustomplot')
         set_kind('static')
         add_files('qcustomplot-source/qcustomplot.cpp')
         add_headerfiles('qcustomplot-source/qcustomplot.h', { public = true })
+        add_includedirs('qcustomplot-source', { public = true })
     ]])
     import('package.tools.xmake').install(package)
   end)
