f63609ed2f72fd67b2379f1d5b22b9292fe3016b
EnergoStalin <ens.stalin@gmail.com>
2024-04-20 21:39:59 +0300

    fix(s21rules): do not run when not coverage mode

--- packages/s/s21rules/rules/libreport.lua
+++ packages/s/s21rules/rules/libreport.lua
@@ -1,5 +1,7 @@
 rule('libreport')
   after_run(function(target)
+    if not is_mode('coverage') then return end
+
     for k, v in pairs(target._DEPS) do
       os.execv('lcov', {
         '-o', path.join(target:targetdir(), k .. '.info'),
