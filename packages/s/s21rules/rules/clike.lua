ded98af125a68ed492b2df8fa35cc220fabdccd6
EnergoStalin <ens.stalin@gmail.com>
2024-04-22 02:07:47 +0300

    refactor(s21rules): use functions instead of nested rules for defining dependant configuration

--- packages/s/s21rules/rules/clike.lua
+++ packages/s/s21rules/rules/clike.lua
@@ -1,23 +1,22 @@
-rule('clike')
-  add_deps('mode.release', 'mode.debug', 'mode.tsan', 'mode.asan')
-  on_config(function(target)
+local function clike(target)
     target:set('warnings', 'allextra', 'error', 'pedantic')
     target:set('toolchains', 'gcc')
+end
 
-    target:set('prefixname', 's21_')
-  end)
-rule_end()
+local function cxx(target)
+     clike(target)
+     target:set('languages', 'cxx17')
+     target:set('toolchains', 'clang')
+end
 
-rule('cxx')
-  add_deps('clike')
-  on_config(function(target)
-    target:set('languages', 'cxx17')
-  end)
-rule_end()
+local function c(target)
+    clike(target)
+    target:set('languages', 'c11')
+end
 
 rule('cxxtest')
-  add_deps('cxx')
   on_config(function(target)
+    cxx(target)
     target:add('packages', 'gtest')
 
     if is_mode('coverage') then
@@ -27,8 +26,9 @@ rule('cxxtest')
 rule_end()
 
 rule('cxxlib')
-  add_deps('cxx', 'mode.coverage')
+  add_deps('mode.coverage')
   on_config(function(target)
+    cxx(target)
     if is_mode('coverage') then
       -- Somehow --coverage is not sufficient for generating .gcno files
       target:add("cxflags", "-fprofile-arcs -ftest-coverage")
@@ -39,16 +39,9 @@ rule('cxxlib')
   end)
 rule_end()
 
-rule('c')
-  add_deps('clike')
-  on_config(function(target)
-    target:set('languages', 'c11')
-  end)
-rule_end()
-
 rule('ctest')
-  add_deps('c')
   on_config(function(target)
+    c(target)
     target:add('packages', 'check')
 
     if is_mode('coverage') then
@@ -58,8 +51,9 @@ rule('ctest')
 rule_end()
 
 rule('clib')
-  add_deps('c', 'mode.coverage')
+  add_deps('mode.coverage')
   on_config(function(target)
+    c(target)
     if is_mode('coverage') then
       -- Somehow --coverage is not sufficient for generating .gcno files
       target:add("cxflags", "-fprofile-arcs -ftest-coverage")
