7c2418901e184efd21c2d4303662bfca82f296b2
EnergoStalin <ens.stalin@gmail.com>
2024-06-02 01:39:05 +0300

    fix(s21rules): use interface getters instead of internal access

--- packages/s/s21rules/rules/libreport.lua
+++ packages/s/s21rules/rules/libreport.lua
@@ -2,7 +2,7 @@ rule('libreport')
   after_run(function(target)
     if not is_mode('coverage') then return end
 
-    for k, v in pairs(target._DEPS) do
+    for k, v in pairs(target.deps()) do
       os.execv('lcov', {
         '-o', path.join(target:targetdir(), k .. '.info'),
         '-c', '-d', v:objectdir(),
@@ -12,7 +12,7 @@ rule('libreport')
     os.execv('sh', {
       '-c',
       'genhtml --ignore-errors unused -exclude \'/usr/*\' -o '
-      .. path.join(target._INFO._INTERPRETER:rootdir(), 'report')
+      .. path.join(os:projectdir(), 'report')
       .. ' '
       .. path.join(target:targetdir(), '*.info')
     })
