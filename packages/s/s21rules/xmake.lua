3e27af03272ca228788ee81078201b0a5230b863
EnergoStalin <ens.stalin@gmail.com>
2024-06-02 01:38:31 +0300

    chore(s21rules): xmake version & description along with homepage

--- packages/s/s21rules/xmake.lua
+++ packages/s/s21rules/xmake.lua
@@ -1,2 +1,7 @@
+set_xmakever("2.9.2")
+
 package('s21rules')
+  set_homepage("https://edu.21-school.ru/")
+  set_description("xmake rules for convinient building according to school rules")
+
   on_install("windows", "macos", "linux", function () end)
