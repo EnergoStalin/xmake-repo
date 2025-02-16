851c97caf71ade464a937f01a24bf5d5256678e2
EnergoStalin <ens.stalin@gmail.com>
2025-02-17 02:01:50 +0300

    fix(qcustomplot): windows build with msvc toolchain

--- packages/q/qcustomplot/xmake.lua
+++ packages/q/qcustomplot/xmake.lua
@@ -1,4 +1,5 @@
 package('qcustomplot')
+  set_kind('library')
   set_homepage('https://www.qcustomplot.com/')
   set_description('Qt C++ widget for plotting and data visualization.')
 
@@ -11,19 +12,19 @@ package('qcustomplot')
     'dcd8226f2787b51de8555c03b3ac9a3e3ba63066f52d31f955336d81ba5d3dfb'
   )
 
-  on_install(function(package)
-    io.writefile('xmake.lua', [[
-      set_languages('cxx11')
+  add_configs('cxxver', {description = 'C++ standard to use for qt > 6 specify > 17', default = 17, type = 'number', readonly = false})
 
-      add_requires('qt5base', 'qt5gui')
+  on_install(function(package)
+    io.writefile('xmake.lua', string.format([[
+      add_rules('mode.debug', 'mode.release')
+      set_languages('cxx%s')
 
       target('qcustomplot')
-        set_kind('shared')
-        add_rules('qt.shared')
+        set_kind('$(kind)')
+        add_rules('qt.$(kind)')
 
         add_defines('QCUSTOMPLOT_COMPILE_LIBRARY')
         add_frameworks('QtPrintSupport')
-        add_packages('qt5base', 'qt5gui')
 
         add_files(
           'qcustomplot.cpp',
@@ -32,6 +33,6 @@ package('qcustomplot')
         
         add_headerfiles('qcustomplot.h', { public = true })
         add_includedirs('.', { public = true })
-    ]])
+    ]], package:config('cxxver')))
     import('package.tools.xmake').install(package)
   end)
