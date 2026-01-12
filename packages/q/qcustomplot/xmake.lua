a89afe53a282bfc2f6d182210b36e6b01a587f84
EnergoStalin <ens.stalin@gmail.com>
2026-01-12 13:12:11 +0300

    fix(qcustomplot): remove specific qt version restraint

--- packages/q/qcustomplot/xmake.lua
+++ packages/q/qcustomplot/xmake.lua
@@ -12,7 +12,7 @@ package('qcustomplot')
     'dcd8226f2787b51de8555c03b3ac9a3e3ba63066f52d31f955336d81ba5d3dfb'
   )
 
-  add_deps('qtbased 6.4.0')
+  add_deps('qtbased')
 
   on_install(function(package)
     io.writefile('xmake.lua', [[
